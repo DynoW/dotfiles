65c2b15451f98633d4801d9f9d07bdeb7ab3030d
DynoW <104089032+DynoW@users.noreply.github.com>
2025-10-05 11:53:42 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo dnf install -y curl zsh fzf zoxide
+sudo dnf update
+dnf copr enable scottames/ghostty
+sudo dnf install -y curl zsh fzf zoxide ghostty
 curl -s https://ohmyposh.dev/install.sh | bash -s
+stow ghostty
+stow posh
+stow zsh
+chsh -s /usr/bin/zsh
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
