ad57e1b2bb0051b0379fcfdde77c9d7b89ea5c01
DynoW <104089032+DynoW@users.noreply.github.com>
2025-10-05 21:23:40 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 sudo dnf update
-dnf copr enable scottames/ghostty
-sudo dnf install -y curl zsh fzf zoxide ghostty
+
+sudo dnf copr enable scottames/ghostty
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+sudo dnf install -y curl zsh fzf zoxide ghostty code syncthing
+flatpak install -y flathub md.obsidian.Obsidian
 curl -s https://ohmyposh.dev/install.sh | bash -s
+
 stow ghostty
 stow posh
 stow zsh
+
 chsh -s /usr/bin/zsh
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+
+#/home/dyno/.local/state/syncthing
+
+#systemctl --user enable syncthing
+#systemctl --user start syncthing
